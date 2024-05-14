bd37a8e3b1f33196ec6f6ea3df57902da0bd7f62
Mehedi-9T9 <mehedi.hasan9t9@gmail.com>
2024-05-14 21:27:46 +0600

    server deploy success

--- index.js
+++ index.js
@@ -29,9 +29,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-        // Send a ping to confirm a successful connection
+
         const bookCollection = client.db("BooksDB").collection("books");
         const borrowCollection = client.db("BooksDB").collection("borrows");
 
@@ -154,8 +152,7 @@ async function run() {
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+
     }
 }
 run().catch(console.log);
